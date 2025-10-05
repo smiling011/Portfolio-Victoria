5c2fef0da5cec24d8a9572dc1339c538943f2b54
smiling011 <victoriasaleck@gmail.com>
2025-10-05 14:47:40 -0500

    Creacion e instalacion de dependencias

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-900 text-white text-4xl">
+      Tailwind funcionando 💅
+    </div>
+  );
+}
